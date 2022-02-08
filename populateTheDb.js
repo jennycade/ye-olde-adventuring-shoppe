989465ee04be43b0d74a0f8f779449bbf2269dd3
Jenny Zonka <jennycade@gmail.com>
2022-02-08 15:01:30 -0800

    Mostly imported all the weapons, weapon properties, and armor from csv files.

--- populateTheDb.js
+++ populateTheDb.js
@@ -17,20 +17,123 @@ async function main() {
   const Shop = require('./models/shop');
 
   // saving
+  // armor
   const createArmor = async (armorRow) => {
-    const armor = new Armor(armorRow);
-    await armor.save();
-    console.log(`New armor ${armor}`);
-  };
+    try {
+      // process
+      const armorObj = {...armorRow};
+      // strip whitespace from armorType
+      armorObj.armorType = armorRow.armorType.trim();
+      // parse booleans
+      armorObj.stealthDisadvantage = armorRow.stealthDisadvantage === 'TRUE';
 
-  // read armor
+      // save
+      const armor = new Armor(armorObj.name);
+      await armor.save();
+      console.log(`New armor: ${armor}`);
+    } catch (err) {
+      console.log(`Error importing armor: ${err}`);
+    }
+  };
+  // weapon properties
+  const createWeaponProperty = async (row) => {
+    try {
+      // process
+      const obj = {...row};
+
+      // save
+      const weaponProperty = new WeaponProperty(obj);
+      await weaponProperty.save();
+      console.log(`New weapon property: ${weaponProperty.name}`);
+    } catch (err) {
+      console.log(`Error importing weapon property: ${err}`);
+    }
+  };
+  // weapons
+  const createWeapon = async (row) => {
+    try {
+      // process
+      const obj = {...row};
+      // convert weapon property string to array of matching WeaponProperty ids
+      if (row.properties !== '') {
+        const props = row.properties.split(', ');
+        const properties = []
+        await props.forEach( async (prop) => {
+          // find the WeaponProperty with matching name
+          const weaponProperty = await WeaponProperty.findOne({name: prop}).exec();
+          if (!weaponProperty) {
+            throw new Error(`Can't find weaponProperty with name ${prop}`);
+          }
+          properties.push(weaponProperty);
+        });
+        obj.properties = properties;
+      }
+      // strip whitespace from damageDice
+      obj.damageDice = obj.damageDice.trim();
+
+      // save
+      const weapon = new Weapon(obj);
+      await weapon.save();
+      console.log(`New weapon: ${weapon.name}`);
+    } catch (err) {
+      console.log(`Error importing weapon: ${err}`);
+    }
+  }
+  
+  // read CSV files
+  // armor
   const importAllArmor = async () => {
     fs.createReadStream('./data/armor.csv')
     .pipe(csv.parse({ headers: true }))
     .on('error', error => console.error(error))
-    .on('data', row => {createArmor(row)})
+    .on('data', async row => { await createArmor(row)})
+    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+  }
+  // weapon properties
+  const importAllWeaponProperties = async () => {
+    fs.createReadStream('./data/weaponProperties.csv')
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', async row => { await createWeaponProperty(row)})
+    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+  }
+  // weapons
+  const importAllWeapons = async () => {
+    fs.createReadStream('./data/weapons.csv')
+    .pipe(csv.parse({ headers: true}))
+    .on('error', error => console.error(error))
+    .on('data', async (row) => { await createWeapon(row)})
     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
   }
 
-  await importAllArmor();
+  // execute
+  // armor
+  const numArmorDocs = await Armor.countDocuments({});
+  if (numArmorDocs === 0) {
+    await importAllArmor();
+  } else {
+    console.log(`Skipping importAllArmor() - ${numArmorDocs} documents`);
+  }
+  // weapon properties
+  const numWeaponPropertyDocs = await WeaponProperty.countDocuments({});
+  if (numWeaponPropertyDocs === 0) {
+    await importAllWeaponProperties();
+  } else {
+    console.log(`Skipping importAllWeaponProperties() - ${numWeaponPropertyDocs} documents`);
+  }
+  // weapons
+  const numWeaponDocs = await Weapon.countDocuments({});
+  if (numWeaponDocs === 0) {
+    await importAllWeapons();
+  } else {
+    console.log(`Skipping importAllWeapons() - ${numWeaponDocs} documents`);
+  }
+
+  // retrying weapons
+  const importedWeapons = await Weapon.find({}, 'name');
+  const importedWeaponNames = importedWeapons.map(obj => obj.name);
+  const unimportedWeapons = await Weapon.find({'name': {'$nin': importedWeaponNames}});
+  console.log(unimportedWeapons);
+
+  // process.exit();
 }
